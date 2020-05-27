10f08c0f97883b4bfcac2c2ba0c3f052409f9264
Artem Safin <artemsafin.webdev@gmail.com>
2020-05-27 19:13:38 +0300

    Добавлена страница СРО

--- src/js/scrollAnimations.js
+++ src/js/scrollAnimations.js
@@ -6,97 +6,92 @@ import detectIt from 'detect-it';
 ScrollMagicPluginGsap(ScrollMagic, gsap);
 
 export default function() {
-    if (window.matchMedia('(max-width: 767px)').matches || detectIt.hasTouch) return;
+    if (window.matchMedia('(max-width: 767px)').matches) return;
 
     const types = document.querySelector('.types');
     const apply = document.querySelector('.how-to-apply');
     const pageFooter = document.querySelector('.page-footer');
     const controller = new ScrollMagic.Controller();
-  
+
     const bgImage = document.querySelector('.fullheight-bg');
     const bgImageOverlay = document.querySelector('.fullheight-bg__overlay');
     const sidelogo = document.querySelector('.sidelogo');
     const container = document.querySelector('.container');
- 
-    if (types) {
-        const introText = types.querySelector('.js-intro-text');
-        const introTextWrapper = types.querySelector('.js-intro-text-wrapper');
-      
 
-        const tl = gsap.timeline();
+    const introText = document.querySelector('.js-intro-text');
+    const introTextWrapper = document.querySelector('.js-intro-text-wrapper');
 
-        gsap.set(introTextWrapper, {
-            minHeight: introText.offsetHeight
-        });
-        gsap.set(introText, { transformOrigin: 'center', position: 'fixed', left: introTextWrapper.getBoundingClientRect().left, top: introTextWrapper.getBoundingClientRect().top + window.pageYOffset, width: '100%' });
-        gsap.set(bgImage, { position: 'fixed' });
+    const tl = gsap.timeline();
 
-        gsap.set(sidelogo, {
-            left: 'auto',
-            right:
-                parseFloat(window.getComputedStyle(container).getPropertyValue('width')) -
-                parseFloat(window.getComputedStyle(container).getPropertyValue('padding-left')) / 2 -
-                parseFloat(window.getComputedStyle(sidelogo).getPropertyValue('width')) / 2
-        });
+    gsap.set(introTextWrapper, {
+        minHeight: introText.offsetHeight
+    });
+    gsap.set(introText, { transformOrigin: 'center', position: 'fixed', left: introTextWrapper.getBoundingClientRect().left, top: introTextWrapper.getBoundingClientRect().top + window.pageYOffset, width: '100%' });
+    gsap.set(bgImage, { position: 'fixed' });
 
-        let resizeTimer = null;
-        window.addEventListener('resize', () => {
-            clearTimeout(resizeTimer);
-            resizeTimer = setTimeout(function() {
-                gsap.set(introText, { transformOrigin: 'center', position: 'fixed', left: introTextWrapper.getBoundingClientRect().left, top: introTextWrapper.getBoundingClientRect().top + window.pageYOffset, width: '100%' });
+    gsap.set(sidelogo, {
+        left: 'auto',
+        display: 'block',
+        right:
+            parseFloat(window.getComputedStyle(container).getPropertyValue('width')) - parseFloat(window.getComputedStyle(container).getPropertyValue('padding-left')) / 2 - parseFloat(window.getComputedStyle(sidelogo).getPropertyValue('width')) / 2
+    });
 
-                gsap.set(sidelogo, {
-                    left: 'auto',
-                    right:
-                        parseFloat(window.getComputedStyle(container).getPropertyValue('width')) -
-                        parseFloat(window.getComputedStyle(container).getPropertyValue('padding-left')) / 2 -
-                        parseFloat(window.getComputedStyle(sidelogo).getPropertyValue('width')) / 2
-                });
-            }, 250);
-        });
+    let resizeTimer = null;
+    window.addEventListener('resize', () => {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function() {
+            gsap.set(introText, { transformOrigin: 'center', position: 'fixed', left: introTextWrapper.getBoundingClientRect().left, top: introTextWrapper.getBoundingClientRect().top + window.pageYOffset, width: '100%' });
 
-        new ScrollMagic.Scene({
-          
-            triggerHook: 'onEnter',
-            duration: '100%'
-        })
-            .setClassToggle(sidelogo, 'hidden')
-            .addTo(controller);
+            gsap.set(sidelogo, {
+                left: 'auto',
+                right:
+                    parseFloat(window.getComputedStyle(container).getPropertyValue('width')) -
+                    parseFloat(window.getComputedStyle(container).getPropertyValue('padding-left')) / 2 -
+                    parseFloat(window.getComputedStyle(sidelogo).getPropertyValue('width')) / 2
+            });
+        }, 250);
+    });
 
-        tl.to(introText, {
+    new ScrollMagic.Scene({
+        triggerHook: 'onEnter',
+        duration: '100%'
+    })
+        .setClassToggle(sidelogo, 'hidden')
+        .addTo(controller);
+
+    tl.to(introText, {
+        duration: 1,
+        autoAlpha: 0,
+        scale: 0.97,
+        ease: 'none'
+    });
+
+    new ScrollMagic.Scene({
+        triggerElement: types,
+        triggerHook: 0,
+        duration: '45%'
+    })
+        .setTween(tl)
+        .addTo(controller);
+
+    const bgTl = gsap.timeline();
+
+    bgTl.to(
+        bgImageOverlay,
+        {
+            opacity: 1,
             duration: 1,
-            autoAlpha: 0,
-            scale: 0.97,
             ease: 'none'
-        });
-
-        new ScrollMagic.Scene({
-            triggerElement: types,
-            triggerHook: 0,
-            duration: '45%'
-        })
-            .setTween(tl)
-            .addTo(controller);
-
-        const bgTl = gsap.timeline();
-
-        bgTl.to(
-            bgImageOverlay,
-            {
-                opacity: 1,
-                duration: 1,
-                ease: 'none'
-            },
-            0
-        );
-        new ScrollMagic.Scene({
-            triggerElement: types,
-            triggerHook: 0,
-            duration: '35%'
-        })
-            .setTween(bgTl)
-            .addTo(controller);
-    }
+        },
+        0
+    );
+    new ScrollMagic.Scene({
+        triggerElement: types,
+        triggerHook: 0,
+        duration: '35%'
+    })
+        .setTween(bgTl)
+        .addTo(controller);
 
     if (apply) {
         const redLabelCircle = apply.querySelector('.how-to-apply__red-label-circle');
--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,3 +1,5 @@
+import { gsap } from 'gsap';
+
 export default function() {
     const tabs = Array.from(document.querySelectorAll('.js-tabs'));
 
@@ -7,17 +9,39 @@ export default function() {
 
         function setActiveTab(index, event) {
             if (event) event.preventDefault();
+
+            const heightBefore = parseFloat(window.getComputedStyle(element).getPropertyValue('height'));
+
+            gsap.set(element, {
+                height: 'auto'
+            });
+
             tabsNav.forEach(btn => btn.classList.remove('active'));
             tabItems.forEach(item => item.classList.remove('active'));
             tabsNav[index].classList.add('active');
             tabItems[index].classList.add('active');
+
+            const heightAfter = parseFloat(window.getComputedStyle(element).getPropertyValue('height'));
+
+            // console.log({
+            //     heightBefore,
+            //     heightAfter
+            // });
+
+            gsap.fromTo(
+                element,
+                { height: heightBefore },
+                {
+                    duration: 0.4,
+                    height: heightAfter,
+                    clearProps: 'all'
+                }
+            );
         }
 
         tabsNav.forEach((btn, btnIndex) => {
             const handler = setActiveTab.bind(btn, btnIndex);
             btn.addEventListener('click', handler);
-
-         
         });
 
         setActiveTab(0);
